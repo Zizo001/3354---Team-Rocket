0a2f148a4cb3c52488b36e5819315313854bd3ef
aPatel-97 <54556824+aPatel-97@users.noreply.github.com>
2019-11-24 12:30:37 -0600

    Add files via upload

--- app/src/main/java/com/example/calenderapp/MainActivity.java
+++ app/src/main/java/com/example/calenderapp/MainActivity.java
@@ -1,61 +1,52 @@
 package com.example.calenderapp;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.support.v4.app.*;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    //defining the valiable of CalenderView type and TextView type
-    CalendarView calender;
+    public static CalendarView calender;
     TextView date_view;
+    public static String date;
+    public static String selectedDate;
+    public static View v;
+    public static String event;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //By ID we can use each compenent
-        // which id is assign
-        //use findViewByID() to get the CalendarView and TextView
 
-       //I'm basically capturing the calender view from activity_main.xml in java class
-        //I'm also capturing the date_view -> the text at top that said "Set the Date"
-        calender = (CalendarView)
-                findViewById(R.id.calender);
-        date_view = (TextView)
-                findViewById(R.id.date_view);
+        calender = (CalendarView) findViewById(R.id.calender);
+        date_view = (TextView) findViewById(R.id.date_view);
 
-        calender
-                .setOnDateChangeListener(
-                        new CalendarView
-                                .OnDateChangeListener() {
+
+        calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
                             @Override
+                            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
 
-                            // In this Listener have one method
-                            // and in this method we will
-                            // get the value of DAYS, MONTH, YEARS
-                            public void onSelectedDayChange(
-                                    CalendarView view,
-                                    int year,
-                                    int month,
-                                    int dayOfMonth)
-                            {
+                                String Date = (month+1) + "-" + dayOfMonth + "-" + year;
 
-                                // Store the value of date with
-                                // format in String type Variable
-                                // Add 1 in month because month
-                                // index is start with 0
-                                String Date
-                                        = dayOfMonth + "-"
-                                        + (month + 1) + "-" + year;
+                                selectedDate = Integer.toString(year) + Integer.toString(month) + Integer.toString(dayOfMonth);
 
+                                v = view;
                                 // set this date in TextView for Display
-                                date_view.setText(Date);
+                                date = Date;
+
+                                Intent i = new Intent(MainActivity.this, dayView.class);
+                                startActivity(i);
+
                             }
                         });
-
     }
+
+
+    public static String getDate(){
+        return date;
+    }
+    public static String getSelectedDate() {return selectedDate;}
+
+
 }
---
+++ app/src/main/java/com/example/calenderapp/dayView.java
@@ -0,0 +1,135 @@
+package com.example.calenderapp;
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class dayView extends AppCompatActivity implements View.OnClickListener {
+
+    TextView dateBox;
+    myDBAdapter helper;
+    public static String selectedDate;
+    private String date;
+    LinearLayout container;
+    public static boolean exists;
+    public static int boxid;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.day_view);
+
+        container = (LinearLayout)findViewById(R.id.llone);
+
+        FloatingActionButton addButton = findViewById(R.id.addButton);
+        addButton.setSize(FloatingActionButton.SIZE_AUTO);
+        addButton.setOnClickListener(this);
+
+       FloatingActionButton homeButton = findViewById(R.id.homeButton);
+        homeButton.setSize(FloatingActionButton.SIZE_AUTO);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(dayView.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        helper = new myDBAdapter(this);
+
+        date = MainActivity.getDate();
+        selectedDate = MainActivity.getSelectedDate();
+
+        dateBox = (TextView) findViewById(R.id.textView);
+        dateBox.setText(date);
+
+        List<String> event = helper.getData(selectedDate);
+        List<Integer> ids = helper.getIds();
+
+       for(String e : event){
+            int i = ids.get(event.indexOf(e));
+            addLine(e, i);
+        }
+
+    }
+
+    public void addLine(final String e, final int uid){
+        LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View addView = layoutInflater.inflate(R.layout.row, null);
+        final AutoCompleteTextView tv = (AutoCompleteTextView)addView.findViewById(R.id.textout);
+        tv.setText(e);
+        tv.setFocusable(false);
+        tv.setCursorVisible(false);
+        tv.setKeyListener(null);
+        tv.setId(uid);
+        final Button removeButton = (Button)addView.findViewById(R.id.remove);
+        removeButton.setId(uid);
+        exists = false;
+
+        final View.OnClickListener thisListener1 = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setBoxId(tv.getId());
+                setExists(true);
+               Intent i = new Intent(dayView.this, eventView.class);
+               startActivity(i);
+            }
+        };
+
+        final View.OnClickListener thisListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = removeButton.getId();
+                helper.delete(id);
+                ((LinearLayout)addView.getParent()).removeView(addView);
+            }
+        };
+
+        removeButton.setOnClickListener(thisListener);
+        tv.setOnClickListener(thisListener1);
+        container.addView(addView);
+    }
+
+    public void setExists(boolean e){
+        exists = e;
+    }
+
+    public static boolean getExists(){
+        return exists;
+    }
+
+    public void setBoxId(int i){
+        boxid = i;
+    }
+
+    public static int getBoxId(){
+        return boxid;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(dayView.this, eventView.class);
+        startActivity(i);
+    }
+
+}
+
