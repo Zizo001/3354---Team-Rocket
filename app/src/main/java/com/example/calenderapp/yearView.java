18c0bee78bb990f41a06bbaeba233cec5c136dbd
Zizo001 <54548478+Zizo001@users.noreply.github.com>
2019-11-25 12:13:00 -0600

    Add files via upload
    Added yearView Class to select from different months

---
+++ app/src/main/java/com/example/calenderapp/yearView.java
@@ -0,0 +1,22 @@
+package com.example.calenderapp;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class yearView {
+    public String monthName;
+    public int monthValue;
+    Button buttonJanuary, buttonFebruary, buttonMarch, buttonApril, buttonJune, buttonJuly
+            , buttonAugust, buttonSeptember, buttonOctober, buttonNovember, buttonDecember;
+
+
+
+}
