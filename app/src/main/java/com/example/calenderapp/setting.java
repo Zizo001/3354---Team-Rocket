b524a401898b565fb5d726389fb2fe66ace9b208
aPatel-97 <54556824+aPatel-97@users.noreply.github.com>
2019-11-25 18:54:12 -0600

    Update setting.java
    added getStyle method

--- app/src/main/java/com/example/calenderapp/setting.java
+++ app/src/main/java/com/example/calenderapp/setting.java
@@ -1,30 +1,38 @@
-package com.example.setting;
+package com.example.calenderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.Toast;
 
-public class setting extends AppCompatActivity {
+public class setting extends AppCompatActivity implements View.OnClickListener {
 
     private Switch myswitch;
+    public static boolean switchStat;
     RadioGroup rg;
     RadioButton rb;
+    static String textStyle = "Regular";
+
     SharedPref sharedPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         sharedPref = new SharedPref(this);
+        Button goBack = (Button)findViewById(R.id.goback);
+
         // Set the theme
-        if(sharedPref.loadNightModeState() == true)
+        if(sharedPref.loadNightModeState() ==true)
         {
+            switchStat = true;
             setTheme(R.style.DarkTheme);
         }
         else setTheme(R.style.AppTheme);
@@ -37,8 +45,10 @@ public class setting extends AppCompatActivity {
 
         // Activate switch
         myswitch = (Switch)findViewById(R.id.myswitch);
+
         if(sharedPref.loadNightModeState() == true)
         {
+            switchStat = true;
             myswitch.setChecked(true);
         }
         myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -58,8 +68,6 @@ public class setting extends AppCompatActivity {
 
     }
 
-
-
     /* This method restart the app after changing theme */
     public void restartApp(){
         Intent i = new Intent(getApplicationContext(), setting.class);
@@ -67,12 +75,25 @@ public class setting extends AppCompatActivity {
         finish();
     }
 
-    /* This method is use to choose text style */
+    /* This method activate radio button to choose text style */
     public void rbclick(View v)
     {
         int radiobuttonid = rg.getCheckedRadioButtonId();
         RadioButton rb = (RadioButton)findViewById(radiobuttonid);
-        Toast.makeText(getBaseContext(), rb.getText(), Toast.LENGTH_LONG).show();
+        textStyle = rb.getText().toString();
+        Toast toast = Toast.makeText(getBaseContext(), rb.getText(), Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 
+    public static String getTextStyle(){
+        return textStyle;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(setting.this, MainActivity.class);
+        startActivity(i);
+    }
 }
+
