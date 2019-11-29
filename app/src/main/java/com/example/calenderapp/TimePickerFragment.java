7d42c0823d504ce5ae97a54312b6cbc8e3b7c3f6
aPatel-97 <54556824+aPatel-97@users.noreply.github.com>
2019-11-29 12:42:11 -0600

    Update TimePickerFragment.java
    added the 12 hr fommat hour method

--- app/src/main/java/com/example/calenderapp/TimePickerFragment.java
+++ app/src/main/java/com/example/calenderapp/TimePickerFragment.java
@@ -18,25 +18,26 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
     public static String ampm;
     public static boolean selectedTime;
     public static int min;
-    public static int hour;
+    public static int h, hour;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current time as the default values for the picker
         selectedTime = false;
         final Calendar c = Calendar.getInstance();
         int hour = c.get(Calendar.HOUR_OF_DAY);
+        int h = c.get(Calendar.HOUR);
         int minute = c.get(Calendar.MINUTE);
 
         // Create a new instance of TimePickerDialog and return it
         return new TimePickerDialog(getActivity(), AlertDialog.THEME_HOLO_DARK, this, hour, minute, false);
     }
-        
-        //method check for time set and sets am and pm 
+
+        //method check for time set and sets am and pm
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         hour = hourOfDay;
         min = minute;
         selectedTime = true;
-        
+
        int hour = hourOfDay;
         int minutes = minute;
         String timeSet = "";
@@ -58,11 +59,11 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
         else
             min = String.valueOf(minutes);
 
-                //creating the time string 
+                //creating the time string
          time = new StringBuilder().append(hour).append(':')
                 .append(min ).append(" ").toString();
-         
-            //creating the am or pm 
+
+            //creating the am or pm
          ampm = new StringBuilder().append(timeSet).toString();
 
          eventView.timeButton.setText(time +" " + ampm);        //setting the timebutton with the selected time
@@ -73,6 +74,7 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
     }
     public static String getAmPm(){return ampm;}
     public static int getHour() {return hour;}
+    public static int get12Hr(){return h;};
     public static int getMin() {return min;}
 
     public static boolean isTimeSelected(){
