efc568a265b1d049e496156ca238d876fdf6cdae
aPatel-97 <54556824+aPatel-97@users.noreply.github.com>
2019-11-30 11:30:16 -0600

    Update myDBAdapter.java
    Added remove reminder call and set remove method

--- app/src/main/java/com/example/calenderapp/myDBAdapter.java
+++ app/src/main/java/com/example/calenderapp/myDBAdapter.java
@@ -1,11 +1,9 @@
-/**
- * This is the Database helper class. This class uses SQLlite
- * This class is used to manipulate the events
- * @Author: Akshar Patel
- * @Documentor: Ramin Nourbakhsh
- */
 package com.example.calenderapp;
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.ContentValues;
 import android.content.Context;
@@ -13,6 +11,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -24,21 +23,20 @@ import androidx.room.util.TableInfo;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class myDBAdapter extends AppCompatActivity {
     myDbHelper myhelper;
-    int uid; //integer id given to the event
-    List<Integer> ids = new ArrayList<>();//ArrayList to hold the event ids
+    public static int alaramId;
+    List<Integer> ids = new ArrayList<>();
+    public static int removeId;
 
     public myDBAdapter(Context context) {
         myhelper = new myDbHelper(context);
     }
 
-    /**
-     * this method inserts data into the database
-     * @return returns the id of the new event
-     */
+        //insert method for database
     public long insertData() {
         SQLiteDatabase db = myhelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -51,34 +49,28 @@ public class myDBAdapter extends AppCompatActivity {
         return id;
     }
 
-    /**
-     * this method checks if two events are schedule for the same time on the same day
-     * @param t time as a string
-     * @param a am or pm
-     * @return true if two events are scheduled for the same time else return false
-     */
+    //check time overlap method to prevent multiple event for same time
     public boolean checkTimeOverlap(String t, String a){
         SQLiteDatabase db = myhelper.getWritableDatabase();
         String[] columns = {myDbHelper.UID, myDbHelper.TIME, myDbHelper.AMPM, myDbHelper.DATE, myDbHelper.EVENT};
         Cursor cursor = db.query(myDbHelper.TABLE_NAME, columns, null, null, null, null, null);
+        String tm = t+a;
 
         while (cursor.moveToNext()) {
             String tie = cursor.getString(cursor.getColumnIndex(myDbHelper.TIME));
             String ap = cursor.getString(cursor.getColumnIndex(myDbHelper.AMPM));
-            String tm = t+a;
+            String date = cursor.getString(cursor.getColumnIndex(myDbHelper.DATE));
             String time = tie + ap;
-            if(time.equals(tm)) {
-                return true;
+            if(date.equals(MainActivity.getSelectedDate())) {
+                if (time.equals(tm)) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    /**
-     *Reads the data in the database and returns it as a list of strings
-     * @param date date of the event in a string format
-     * @return the list of strings
-     */
+        //get data method will read database and put event in the List and return it
     public List<String> getData(String date) {
         SQLiteDatabase db = myhelper.getWritableDatabase();
         String[] columns = {myDbHelper.UID, myDbHelper.TIME, myDbHelper.DATE, myDbHelper.AMPM, myDbHelper.EVENT};
@@ -91,26 +83,28 @@ public class myDBAdapter extends AppCompatActivity {
             String dat = cursor.getString(cursor.getColumnIndex(myDbHelper.DATE));
             String event = cursor.getString(cursor.getColumnIndex(myDbHelper.EVENT));
             String ap = cursor.getString(cursor.getColumnIndex(myDbHelper.AMPM));
-
+            setAlaramId(cid);
             if(dat.equals(date)) {
-                events.add(tie + ap + "     " + event);
+                events.add(tie + ap + "  " + event);
                 ids.add(cid);
             }
         }
         return events;
     }
 
+    public void setAlaramId(int aId) {
+        alaramId = aId;
+    }
+
+    public static int getAlaramId() {
+        return alaramId;
+    }
+
     public List<Integer> getIds(){
         return ids;
     }
 
-    /**
-     * update an event
-     * @param id id given to the event by the database
-     * @param e event
-     * @param t time
-     * @param a am or pm
-     */
+        //update event will take in the event uid and the new event and time and update
     public void updateEvent(int id, String e, String t, String a){
         SQLiteDatabase db = myhelper.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -122,21 +116,29 @@ public class myDBAdapter extends AppCompatActivity {
         getData(MainActivity.getSelectedDate());
     }
 
-    /**
-     * deletes an event based on it's id
-     * @param id id given to event by database
-     */
+        //delete will take in the id and delete
     public void delete(int id){
         SQLiteDatabase db = myhelper.getWritableDatabase();
         String[] whereArgs = {String.valueOf(id)};
         db.delete(myDbHelper.TABLE_NAME, myDbHelper.UID+" = ?", whereArgs);
+        eventView ev = eventView.getInstance();
+        if(ev != null){
+            ev.removeReminder(id);
+        }else{
+            setRemoveId(id);
+        }
     }
 
+    public void setRemoveId(int i){
+        removeId = i;
+    }
+    public static int getRemoveId(){return removeId;}
 
-    static class myDbHelper extends SQLiteOpenHelper {
+        //creating a new table database using id,time,ampm,date,event
+        class myDbHelper extends SQLiteOpenHelper {
         private static final String DATABASE_NAME = "myDatabase";
         private static final String TABLE_NAME = "myTable";
-        private static final int DATABASE_Version = 5;
+        private static final int DATABASE_Version = 13;
         private static final String UID = "_id";
         private static final String EVENT = "event";
         private static final String DATE = "date";
