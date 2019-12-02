5eea29ba1e5543b02db074c48914b1a5dbf1c27d
cheshiregift <xinqiuhuang@gmail.com>
2019-12-02 09:23:11 -0600

    Update yearView.java

--- app/src/main/java/com/example/calenderapp/yearView.java
+++ app/src/main/java/com/example/calenderapp/yearView.java
@@ -1,43 +1,51 @@
 package com.example.calenderapp;
-import android.app.DatePickerDialog;
-import android.content.ContentValues;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.GridLayout;
-import android.widget.LinearLayout;
-import android.widget.NumberPicker;
 import android.widget.TextView;
-import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import java.util.Calendar;
-
-import static com.example.calenderapp.MainActivity.v;
 
 public class yearView extends AppCompatActivity {
     public String monthName;
     public int monthValue;
     public static Button buttonJanuary;
+    SharedPref sharedPref;
 
 
-    //method to incorporate button functionality 
+    //method to incorporate button functionality
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        sharedPref = new SharedPref(this);//creating an instance of sharedPref class
+
+        //if the night mode is on
+        if(sharedPref.loadNightModeState() == true)
+        {
+            //set the calender application to dark theme
+            setTheme(R.style.DarkTheme);
+        }
+        else setTheme(R.style.AppTheme);//if not, set the calender application to app theme
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.year_view);
         Calendar c = Calendar.getInstance();
 
         int year = c.get(Calendar.YEAR);
         final TextView yearText = (TextView)findViewById(R.id.yearText);
-        yearText.setText(Integer.toString(year));                       
+        yearText.setText(Integer.toString(year));
 
-     //each month has a corresponding button with values from 0-11 assigned to the respective activity
-     //and sets monthSelected to true   
+        //each month has a corresponding button with values from 0-11 assigned to the respective activity
+        //and sets monthSelected to true
         buttonJanuary = (Button) findViewById(R.id.buttonJanuary);
         buttonJanuary.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -158,5 +166,22 @@ public class yearView extends AppCompatActivity {
     private void changeView(){
         Intent intent = new Intent(yearView.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public static class AlarmReceiver extends BroadcastReceiver {
+        //this method will be invoked when the time on an event occurs
+        @Override
+        public void onReceive(Context context, Intent intent){
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "myCalendar")
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setContentTitle("Calendar App")
+                    .setContentText("Event Reminder!")
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+            notificationManager.notify(154, builder.build());   //building the notification with the info
+        }
     }
 }
