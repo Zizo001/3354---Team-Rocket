ae83a8a688903e153c3cea85bfd89d6cdd6ecfd7
Veerendranath Korrapati <vkpati20@gmail.com>
2019-12-04 13:25:28 -0600

    Update myDBAdapterTest.java

--- app/src/test/java/com/example/calenderapp/myDBAdapterTest.java
+++ app/src/test/java/com/example/calenderapp/myDBAdapterTest.java
@@ -12,10 +12,22 @@ public class myDBAdapterTest {
         Assert.assertTrue(true);        //Aksharkumar
     }
 
-    @Test
-    public void checkTimeOverlapTrue() {
-        Assert.assertTrue(true);
-    }       //Veeru
+     @Test
+    public void checkTimeOverlap() {
+        String dbDate = "10/20/2019";
+        String dbAMPM = "AM";
+        String dbTime = "2:30";
+
+        String date = "10/20/2019";
+        String ampm = "AM";
+        String time = "2:30";
+
+        String finalTime = dbTime + dbAMPM;
+        String finalTime2 = time + ampm;
+
+        boolean result = finalTime.equals(finalTime2);
+        assertEquals(true, result);
+    }//Veerendranath Korrapati
 
     @Test
     public void checkTimeOverlapFalse() {
