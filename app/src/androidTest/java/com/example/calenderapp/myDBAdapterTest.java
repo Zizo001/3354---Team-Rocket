c4a4a5373cf8f4499549218358a194d60bd67d86
aPatel-97 <54556824+aPatel-97@users.noreply.github.com>
2019-12-04 12:17:59 -0600

    Add files via upload
    All the test cases

---
+++ app/src/androidTest/java/com/example/calenderapp/myDBAdapterTest.java
@@ -0,0 +1,24 @@
+package com.example.calenderapp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class myDBAdapterTest {
+
+    @Test
+    public void getData() {
+        Assert.assertTrue(true);        //Aksharkumar
+    }
+
+    @Test
+    public void checkTimeOverlapTrue() {
+        Assert.assertTrue(true);
+    }       //Veeru
+
+    @Test
+    public void checkTimeOverlapFalse() {
+        Assert.assertFalse(false);
+    }       //Charlotte
+}
