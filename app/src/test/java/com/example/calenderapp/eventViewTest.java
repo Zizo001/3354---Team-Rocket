21cd6a77b23ce8fbcd66204c8c0597ac3a74bf1f
aPatel-97 <54556824+aPatel-97@users.noreply.github.com>
2019-12-04 12:15:54 -0600

    Add files via upload
    Added the test cases for everyone on team

---
+++ app/src/test/java/com/example/calenderapp/eventViewTest.java
@@ -0,0 +1,14 @@
+package com.example.calenderapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class eventViewTest{
+
+    @Test
+    public void getInstanceTest() {
+        assertNull(eventView.eventInstance);
+    }           //Ramin
+
+}
